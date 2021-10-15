ac90a9050996ad2db664f0ca2dc3713fceda1a28
lubish <louisebishopdev@gmail.com>
2021-10-15 19:58:09 +0100

    Initial Commit

---
+++ Stretch+Paws/Stretch_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stretch_PawsApp.swift
+//  Stretch+Paws
+//
+//  Created by Louise Bishop on 15/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Stretch_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
